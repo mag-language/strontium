dcc91d4af58d0d7469c49aa128b2cd035bd4fcef
Moritz Riede <mriede@tuta.io>
2020-03-08 09:25:58 +0100

    Update instruction encoder; incorporate new memory methods

--- src/machine/instruction/encode/mod.rs
+++ src/machine/instruction/encode/mod.rs
@@ -6,6 +6,47 @@ use crate::types::Location;
 
 use byteorder::{LittleEndian, WriteBytesExt};
 
+pub fn encode(header: Header, instructions: Vec<Instruction>) -> Vec<u8> {
+	let mut accumulator = vec![];
+
+	accumulator.append(&mut header.into());
+
+	for instruction in instructions {
+		accumulator.append(&mut instruction.into());
+	}
+
+	accumulator
+}
+
+pub struct Header {
+	/// The version(s) of Strontium compatible with this program.
+	pub version: String,
+	/// Because the virtual machine doesn't have a type system,
+	/// a constant is simply an array of bytes.
+	pub constants: Vec<Vec<u8>>, 
+}
+
+impl Into<Vec<u8>> for Header {
+	fn into(self) -> Vec<u8> {
+		let mut accumulator = vec![];
+
+		// Append the magic "strontium" string to the start of the executable.
+		accumulator.append(&mut String::from("strontium").into_bytes());
+
+		// Push the total number of constants.
+		accumulator.push(self.constants.len() as u8);
+
+		// Now, for each constant, write its length and the value into the vector.
+		for mut constant in self.constants {
+			accumulator.push(constant.len() as u8);
+			accumulator.append(&mut constant);
+		}
+
+		// Return the encoded header.
+		accumulator
+	}
+}
+
 impl Into<Vec<u8>> for Instruction {
 	fn into(self) -> Vec<u8> {
 		match self {
@@ -103,6 +144,8 @@ impl Into<Vec<u8>> for Instruction {
 					SUBTRACT => accumulator.push(0x02),
 					MULTIPLY => accumulator.push(0x03),
 					DIVIDE   => accumulator.push(0x04),
+					POWER    => accumulator.push(0x05),
+					MODULO   => accumulator.push(0x06),
 				}
 
 				accumulator.append(&mut vec![operand1, operand2, destination]);
@@ -139,16 +182,17 @@ impl Into<Vec<u8>> for Instruction {
 				accumulator.push(0x07);
 
 				match method {
-					AND { .. } => accumulator.push(0x01),
-					OR  { .. } => accumulator.push(0x02),
-					XOR { .. } => accumulator.push(0x03),
-					NOT { .. } => accumulator.push(0x04),
-					LSH { .. } => accumulator.push(0x05),
-					RSH { .. } => accumulator.push(0x06),
-					GROW { .. } => accumulator.push(0x07),
-					SHRINK { .. } => accumulator.push(0x08),
-					SET { .. } => accumulator.push(0x09),
-					UNSET { .. } => accumulator.push(0x0A),
+					AND { .. } 	  => accumulator.push(0x01),
+					OR  { .. } 	  => accumulator.push(0x02),
+					XOR { .. } 	  => accumulator.push(0x03),
+					NOT { .. } 	  => accumulator.push(0x04),
+					LSH { .. } 	  => accumulator.push(0x05),
+					RSH { .. } 	  => accumulator.push(0x06),
+					GROW { .. }   	 => accumulator.push(0x07),
+					SHRINK { .. } 	 => accumulator.push(0x08),
+					SET { .. } 	  	 => accumulator.push(0x09),
+					SET_RANGE { .. } => accumulator.push(0x0A),
+					UNSET { .. }     => accumulator.push(0x0B),
 				}
 
 				match method {
@@ -222,6 +266,20 @@ impl Into<Vec<u8>> for Instruction {
         					.expect("Unable to write memory address into accumulator");
 
         				accumulator.push(value)
+					},
+
+					SET_RANGE { address, values } => {
+						accumulator
+        					.write_u64::<LittleEndian>(address)
+        					.expect("Unable to write memory address into accumulator");
+        				
+						accumulator
+        					.write_u32::<LittleEndian>(values.len() as u32)
+        					.expect("Unable to write range length into accumulator");
+
+        				for value in values {
+        					accumulator.push(value);
+        				}
 					},
 
 					UNSET { address } => {
