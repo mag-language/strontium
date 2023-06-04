9070e22d3d1af93269d583d26a14f118b3f73ad1
Moritz Riede <moritz@mriede.com>
2023-06-04 23:58:52 +0200

    chore: import modules and centralize strontium error type

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,3 +1,6 @@
+pub mod value;
+pub mod program;
+
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Location {
 	Memory(MemoryAddress),
@@ -9,3 +12,17 @@ pub type MemoryAddress = u64;
 
 /// A reference to a floating-point register
 pub type RegisterAddress = u8;
+
+#[derive(Debug, PartialEq)]
+pub enum StrontiumError {
+	/// A division by zero has occured.
+	DivisionByZero,
+	/// An invalid memory or register address has been accessed.
+	OutOfBounds,
+	/// The machine encountered an invalid operation code.
+	IllegalOpcode(u8),
+	/// The bytecode sequence has ended unexpectedly.
+	UnexpectedEof,
+	/// The type of a register does not match the provided value.
+	TypeMismatch,
+}
