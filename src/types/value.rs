3af534fbae07511f84f5876df9afe5d8b9a10a16
Moritz Riede <moritz@mriede.com>
2023-09-20 01:34:35 +0200

    chore: create a tag for types

---
+++ src/types/value.rs
@@ -0,0 +1,29 @@
+#[derive(Debug, Clone, PartialEq)]
+pub enum ValueType {
+    /// A signed 8-bit integer
+    Int8,
+    /// A signed 16-bit integer
+    Int16,
+    /// A signed 32-bit integer
+    Int32,
+    /// A signed 64-bit integer
+    Int64,
+    /// An unsigned 8-bit integer
+    UInt8,
+    /// An unsigned 16-bit integer
+    UInt16,
+    /// An unsigned 32-bit integer
+    UInt32,
+    /// An unsigned 64-bit integer
+    UInt64,
+    /// A 32-bit floating point value
+    Float32,
+    /// A 64-bit floating point value
+    Float64,
+    /// A sequence of UTF-8 graphemes
+    String,
+    /// A list of values.
+    Array,
+    // A key-value assignment of strings and values.
+    Map,
+}
