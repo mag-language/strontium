b4dd62d9082f89f68d3d3afe6411c54de117cf6f
Moritz Riede <moritz@mriede.com>
2023-10-15 22:49:00 +0200

    doc: add root doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
-// #![feature(custom_attribute)]
-
-// #[macro_use] extern crate pest_derive;
-
-// pub mod assembler;
+//! ![Strontium](https://s3.fr-par.scw.cloud/strontium.dev/banner_slim.svg)
+//!
+//! Strontium is a bytecode interpreter for static and dynamic programming languages.
+//!
+//! It is built primarily to support Mag as the underlying, executive portion of the language engine.
+//! Any Mag source code first runs through the compiler, is then compiled to Strontium bytecode and
+//! finally interpreted by this virtual machine.
 pub mod types;
 pub mod machine;
 pub mod repl;
