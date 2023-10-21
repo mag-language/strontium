3bb84d0fc2510be2022c74c20c269abb79c5d099
Moritz Riede <moritz@mriede.com>
2023-10-21 02:06:52 +0200

    chore: remove register value and deprecated memory types

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,18 +1,6 @@
 pub mod value;
 pub mod program;
 
-#[derive(Debug, Copy, Clone, PartialEq)]
-pub enum Location {
-	Memory(MemoryAddress),
-	Register(RegisterAddress),
-}
-
-/// A reference to a location in memory
-pub type MemoryAddress = u64;
-
-/// A reference to a floating-point register
-pub type RegisterAddress = u8;
-
 #[derive(Debug, PartialEq)]
 pub enum StrontiumError {
 	/// A division by zero has occured.
