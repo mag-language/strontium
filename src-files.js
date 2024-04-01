c2e215f8933d0b1d6fb4edac53b888ceffc6400b
spectrachrome <spectrachrome@users.noreply.github.com>
2024-04-01 05:04:04 +0000

    deploy: 2cae781d1033cc2843321952951ad51fea1717c3

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
 var srcIndex = new Map(JSON.parse('[\
-["strontium",["",[["machine",[["bytecode",[["parselets",[],["mod.rs"]]],["decode.rs","encode.rs","mod.rs"]],["instruction",[["executors",[],["calculate.rs","call.rs","halt.rs","interrupt.rs","load.rs","mod.rs","ret.rs"]]],["mod.rs"]]],["mod.rs","opcode.rs","register.rs"]],["types",[],["mod.rs","program.rs","value.rs"]]],["lib.rs"]]]\
+["strontium",["",[["machine",[["bytecode",[["parselets",[],["mod.rs"]]],["decode.rs","encode.rs","mod.rs"]],["instruction",[["executors",[],["calculate.rs","call.rs","halt.rs","interrupt.rs","load.rs","mod.rs","ret.rs"]]],["mod.rs"]]],["mod.rs","opcode.rs","register.rs"]],["types",[],["error.rs","mod.rs","program.rs","value.rs"]]],["lib.rs"]]]\
 ]'));
 createSrcSidebar();
--- strontium/types/sidebar-items.js
+++ strontium/types/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["StrontiumError"],"mod":["program","value"]};
+window.SIDEBAR_ITEMS = {"mod":["error","program","value"]};
