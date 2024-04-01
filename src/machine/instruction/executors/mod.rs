bd85ce111d035048928ceb257b239c0f2511d54f
Moritz Riede <moritz@mriede.com>
2024-04-01 03:56:28 +0200

    chore: use call, calc and return modules

--- src/machine/instruction/executors/mod.rs
+++ src/machine/instruction/executors/mod.rs
@@ -1,9 +1,13 @@
 mod calculate;
+mod call;
 mod halt;
 mod interrupt;
 mod load;
+mod ret;
 
 pub use self::halt::*;
 pub use self::load::*;
 pub use self::interrupt::*;
+pub use self::call::*;
 pub use self::calculate::*;
+pub use self::ret::*;
