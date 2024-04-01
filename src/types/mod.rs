3a90fc148ecb1bd55b1c76a2b635390b871aba91
Moritz Riede <moritz@mriede.com>
2024-04-01 03:59:23 +0200

    chore: add a few new error types

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,5 +1,9 @@
+use crate::machine::bytecode::BytecodeError;
+
 pub mod value;
 pub mod program;
+
+pub use self::value::*;
 
 #[derive(Debug, PartialEq)]
 pub enum StrontiumError {
@@ -7,6 +11,7 @@ pub enum StrontiumError {
 	DivisionByZero,
 	/// An invalid memory or register address has been accessed.
 	OutOfBounds,
+	BytecodeError(BytecodeError),
 	/// The machine encountered an invalid operation code.
 	IllegalOpcode(u8),
 	/// The bytecode sequence has ended unexpectedly.
@@ -14,4 +19,13 @@ pub enum StrontiumError {
 	/// The type of a register does not match the provided value.
 	TypeMismatch,
 	InvalidUtf8String,
+	EmptyCallStack,
+	/// The user has not called `strontium.init()`.
+	Uninitialized,
+}
+
+impl From<BytecodeError> for StrontiumError {
+	fn from(e: BytecodeError) -> Self {
+		Self::BytecodeError(e)
+	}
 }
